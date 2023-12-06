f5d2dfc5a988404e478e5dbeb9da258c87635f34
Dhirendra Singh <94003852+dhirendra2834@users.noreply.github.com>
2023-12-06 11:10:23 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,27 @@
+
+
+# def greet(name):
+#     print("Good Morning",name)
+
+# greet('Anuj')
+# greet('Dhirendra')
+# greet('Vikram Sir')
+# greet('Vishnu ji ')
+
+# def wish(name, food="Fulki"):
+#     print("Good Morning", name)
+#     print ("Eat Food", food)
+
+#     return name
+
+# person=wish("Dhirendra", 'Samosha')
+# print(person)
+
+def welcome(name):
+    print(name)
+    for x in name:
+        print(x)
+
+
+names=['Dhirendra','Vikram','om']
+welcome(names)
