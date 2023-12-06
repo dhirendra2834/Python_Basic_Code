526ac7a2c240e73c8cd187c5a696c4f2bc2d82ac
Dhirendra Singh <94003852+dhirendra2834@users.noreply.github.com>
2023-12-06 11:11:12 +0530

    Add files via upload

---
+++ Range.py
@@ -0,0 +1,6 @@
+#In range function tell starting value default 0 and ending value less than range limit 
+a=range(10)
+print(a) #print=range(0,10) range(start_value,Limit)
+
+b=list(range(0,10,2)) # take value( starting to less than limit )
+print(b)
