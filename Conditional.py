0dded0dad4b581856d167d4e0ae4e6cff654b5e6
Dhirendra Singh <94003852+dhirendra2834@users.noreply.github.com>
2023-12-06 11:09:19 +0530

    Add files via upload

---
+++ Conditional.py
@@ -0,0 +1,20 @@
+# age=int(input("Enter your age"))
+
+# if age>18:
+#  print("you can vote ")
+
+# elif age==18:
+#    print("you can vote next year ")
+# else:
+#     print("you can not vote")
+
+
+# odd Even find number 
+n=int(input("Enter the value  "))
+
+if n%2==0:
+    print("This Number is Even ")
+elif n<0:
+    print("This is negative Number  ")
+else    :
+    print("This is odd Number  ")
