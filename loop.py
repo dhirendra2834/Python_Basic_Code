80ea3c9f03309ba817aac02abfd4cb349c0e7ce6
Dhirendra Singh <94003852+dhirendra2834@users.noreply.github.com>
2023-12-06 11:09:57 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,36 @@
+# for loop statements
+# for x in range(10):
+#     print(2*x,end=", ")
+#     print()
+
+# a=['Dhirendra', "Mohit", "Sagar"]
+# for name in a:
+#     print(name)
+
+# while loop statements
+# n=5
+
+# while n>0:
+#     print(n)
+#     n-=1
+
+# break and continue 
+
+# break
+# for x in range(10):
+#     if x>5:
+#         break
+#     print(x)
+
+    # continue
+# x=5
+
+for x in range(10):
+    if x==3:
+        continue
+    print(x)
+ 
+    
+
+
+
