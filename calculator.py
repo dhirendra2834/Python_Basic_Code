7855800fef1b070930806d40feba90153eb0d593
Dhirendra Singh <94003852+dhirendra2834@users.noreply.github.com>
2023-12-06 11:09:03 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def sum(a,b):
+    return a+b
+
+def Minus(a,b):
+    return a-b
+
+def Multiply(a,b):
+    return a*b
+
+def Divide(a,b):
+    return a/b
+
+
+
+print("Sum of ",sum(98,34))
+print("Sum of ",Minus(9,5))
+print("Sum of ",Multiply(8,4))
+print("Sum of ",Divide(34,8))
