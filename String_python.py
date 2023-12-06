bfe93c87b44081907a1837eac256dfb5258188bb
Dhirendra Singh <94003852+dhirendra2834@users.noreply.github.com>
2023-12-06 11:11:34 +0530

    Add files via upload

---
+++ String_python.py
@@ -0,0 +1,21 @@
+#  multiple line in string 
+# name ='''
+# My name is Dhirendra Singh
+# Currently my living in Satna(M.p.)
+# '''
+
+# print(name)
+
+
+name="Dhirendra"
+
+print(name[0])
+
+# slcing apply
+# slcing means apply indexing wise print like range give in string 
+
+print(name[0:9:2])
+
+
+for x in name:
+    print(x)
