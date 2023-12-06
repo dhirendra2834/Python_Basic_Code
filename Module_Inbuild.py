8b73f08eb757bb8a5f080a922d053d838410786c
Dhirendra Singh <94003852+dhirendra2834@users.noreply.github.com>
2023-12-06 11:10:57 +0530

    Add files via upload

---
+++ Module_Inbuild.py
@@ -0,0 +1,19 @@
+# import Math Folder in which many () included
+# 1st way
+# import math
+
+# print(math.pi)
+
+# 2nd ways
+# if 2 or 3 function used then can import module like
+
+# from math import cos, sin , pi
+
+# print(cos(3))
+# print(sin(4))
+# print(pi)
+
+# 3rd way module import used 
+from math import *
+
+print(cos(3))
